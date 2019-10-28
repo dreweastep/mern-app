f60b7b45c47db987c868d9c952e21c63993bfb56
Eastep <easandb@dunwoody.edu>
2019-10-27 22:05:04 -0500

    mongo connection and apis

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV: "<insert-mongo-srv>",
+    MONGO_SRV: "mongodb://DrewEastep:mongoPassword@192.168.45.13:27017/admin",
     JWT_SECRET: "<insert-jwt-secret>",
     CLOUDINARY_URL: "<insert-cloudinary-url>",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
-function Home() {
+import React from 'react';
+import axios from 'axios';
+
+function Home({products}) {
+  console.log(products)
+  // React.useEffect(() => {
+  //   getProducts()
+  // }, [])
+
+  // async function getProducts() {
+  //   const url = 'http://localhost:3000/api/products';
+  //   const response = await axios.get(url);
+  //   console.log(response.data)
+  // }
+
   return <>home</>;
+}
+
+Home.getInitialProps = async () => {
+  //fetch data on server
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+
+  //return response data as an objext 
+  return { products: response.data };
+
+  //note: this object will be merged with initial props
 }
 
 export default Home;
