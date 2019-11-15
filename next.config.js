fbe9f74ad9f978f4b066f7df878ec2988b27ccac
Eastep <easandb@dunwoody.edu>
2019-11-14 18:37:13 -0600

    added payment processing

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
     MONGO_SRV: "mongodb://DrewEastep:mongoPassword@192.168.45.13:27017/admin",
     JWT_SECRET: "asdfasdfasdf",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/d1776east/image/upload",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
+    STRIPE_SECRET_KEY: "sk_test_aSHmY7E9Gm0mIbLnWrKykiRe000f4ELoCd"
   }
 };
